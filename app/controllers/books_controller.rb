15161628e25dfb33d2298ec2847d75682d366979
HidekiShiba <hshiba033@gmail.com>
2020-11-18 05:35:10 +0000

    [Add] 投稿データの保存/ストロングパラメータ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,13 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def index
@@ -15,5 +20,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
