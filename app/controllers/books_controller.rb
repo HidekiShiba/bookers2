fd58f043475f0c6b0a02465a0d22b12d00dbd922
HidekiShiba <hshiba033@gmail.com>
2020-11-19 08:39:16 +0000

    [Add] User/Bookモデルバリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book)
     else 
-      render :index
+      render :new
     end
   end
 
