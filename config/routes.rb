842ef764c1c496c42f55e6c9270a056262d5554a
HidekiShiba <hshiba033@gmail.com>
2020-11-19 11:01:46 +0000

    [Update] サインアップ/ログイン後のリダイレクト

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about" => "homes#about"
-  devise_for :users, controllers: {
-        registrations: 'users/registrations'
-}
+  devise_for :users, controllers: { registrations: 'users/registrations',
+                                    sessions: 'users/sessions' }
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
