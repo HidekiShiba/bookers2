81db34986b95a82f3c15a3541d109393334afcc3
HidekiShiba <hshiba033@gmail.com>
2020-11-21 14:47:21 +0000

    [Update] ヘッダーレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get "home/about" => "homes#about"
+  get 'home/about' => 'homes#about'
   devise_for :users, controllers: { registrations: 'users/registrations',
                                     sessions: 'users/sessions' }
   resources :users, only: [:show, :index, :edit, :update]
